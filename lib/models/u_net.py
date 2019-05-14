7c1975564f563b4529437b418364233892cf99d0
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-14 17:06:39 -0500

    write mobile net

--- lib/models/u_net.py
+++ lib/models/u_net.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class UNet(nn.Module):
+class UNetF1(nn.Module):
     def __init__(self, n_channels, n_classes):
-        super(UNet, self).__init__()
+        super(UNetF1, self).__init__()
         self.inc = inconv(n_channels, 64)
         self.down1 = down(64, 128)
         self.down2 = down(128, 256)
@@ -16,6 +16,7 @@ class UNet(nn.Module):
         self.up3 = up(256, 64)
         self.up4 = up(128, 64)
         self.outc = outconv(64, n_classes)
+        self.fc = nn.Linear(64, 1)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -27,11 +28,12 @@ class UNet(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-        x = self.outc(x)
-        return x
+        print(x.shape)
+        raise RuntimeError
+        return self.outc(x), self.fc(x)
 
 
-class UNetF1(nn.Module):
+class UNet(nn.Module):
     def __init__(self, n_channels, n_classes):
         super(UNet, self).__init__()
         self.inc = inconv(n_channels, 64)
