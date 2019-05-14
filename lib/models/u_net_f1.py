eb87b043a6fbc5a4572bd5cd7f4af0b46ac7b592
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-14 17:33:09 -0500

    implement f1

---
+++ lib/models/u_net_f1.py
@@ -0,0 +1,36 @@
+from lib.models.u_net import inconv, down, up, outconv
+import torch.nn as nn
+
+
+class UNetF1(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(UNetF1, self).__init__()
+        self.inc = inconv(n_channels, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up(1024, 256)
+        self.up2 = up(512, 128)
+        self.up3 = up(256, 64)
+        self.up4 = up(128, 64)
+        self.outc = outconv(64, n_classes)
+        self.fc = nn.Linear(1085440, 1)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x_ = x.view(x.size(0), -1)
+        return self.outc(x), self.fc(x_)
+
+
+def build_u_net_f1(n_classes=21):
+    net = UNetF1(n_channels=3, n_classes=n_classes)
+    return net
