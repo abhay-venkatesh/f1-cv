6d892c62e3ee08a173b6ea47a59a1eadb35ffa1c
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-19 08:27:56 -0500

    stop for now

--- lib/agents/coco_stuff_evaluator.py
+++ lib/agents/coco_stuff_evaluator.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 import importlib
 import torch
 from math import floor
+from torchvision import transforms
 
 # from pycocotools.cocostuffhelper import segmentationToCocoResult
 
@@ -30,24 +31,50 @@ class COCOStuffEvaluator(Agent):
 
         model.eval()
         with torch.no_grad():
-            for X in testset:
-                windows, h_overlaps, w_overlaps = self._get_windows(X)
-                X = torch.stack(windows)
+            for img in testset:
+                _, h, w = img.shape
+                windows, h_overlaps, w_overlaps = self._get_windows(img)
+
+                X = torch.stack(windows).to(self.device)
                 Y_, _ = model(X)
                 _, predicted = torch.max(Y_.data, 1)
-                print(predicted)
+                seg = self._construct_mask(predicted, h, w)
+
+                X = torch.stack(h_overlaps).to(self.device)
+                Y_, _ = model(X)
+                _, predicted = torch.max(Y_.data, 1)
+                seg = self._apply_h_overlaps(predicted, seg, w)
+
+                X = torch.stack(w_overlaps).to(self.device)
+                Y_, _ = model(X)
+                _, predicted = torch.max(Y_.data, 1)
+                seg = self._apply_w_overlaps(predicted, seg, h)
+
+                seg_img = transforms.ToPILImage()(seg)
+                seg_img.save(Path(self.config["outputs folder"]))
                 raise RuntimeError
 
-    def _get_windows(self, X):
-        """
-        [
-            [1, 1, 0, 0],
-            [1, 1, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-        ]
-        """
-        c, h, w = X.shape
+    def _construct_mask(self, predicted, h, w):
+        seg = torch.zeros((h, w))
+        num_h_fits = h / self.WINDOW_SIZE
+        num_w_fits = w / self.WINDOW_SIZE
+        k = 0
+        for i in range(0, floor(num_h_fits)):
+            for j in range(0, floor(num_w_fits)):
+                h1, h2 = i * self.WINDOW_SIZE, (i + 1) * self.WINDOW_SIZE
+                w1, w2 = j * self.WINDOW_SIZE, (j + 1) * self.WINDOW_SIZE
+                seg[h1:h2, w1:w2] = predicted[k, :, :]
+                k += 1
+        return seg
+
+    def _apply_h_overlaps(self, predicted, seg, w):
+        pass
+
+    def _apply_w_overlaps(self, predicted, seg, h):
+        pass
+
+    def _get_windows(self, img):
+        _, h, w = img.shape
         num_h_fits = h / self.WINDOW_SIZE
         num_w_fits = w / self.WINDOW_SIZE
         windows = []
@@ -55,20 +82,20 @@ class COCOStuffEvaluator(Agent):
             for j in range(0, floor(num_w_fits)):
                 h1, h2 = i * self.WINDOW_SIZE, (i + 1) * self.WINDOW_SIZE
                 w1, w2 = j * self.WINDOW_SIZE, (j + 1) * self.WINDOW_SIZE
-                windows.append(X[:, h1:h2, w1:w2])
+                windows.append(img[:, h1:h2, w1:w2])
 
         h_overlaps = []
         if not num_h_fits.is_integer():
             for j in range(0, floor(num_w_fits)):
                 h1, h2 = h - self.WINDOW_SIZE, h
                 w1, w2 = j * self.WINDOW_SIZE, (j + 1) * self.WINDOW_SIZE
-                windows.append(X[:, h1:h2, w1:w2])
+                windows.append(img[:, h1:h2, w1:w2])
 
         w_overlaps = []
         if not num_w_fits.is_integer():
             for i in range(0, floor(num_h_fits)):
                 h1, h2 = i * self.WINDOW_SIZE, (i + 1) * self.WINDOW_SIZE
                 w1, w2 = w - self.WINDOW_SIZE, w
-                windows.append(X[:, h1:h2, w1:w2])
+                windows.append(img[:, h1:h2, w1:w2])
 
         return windows, h_overlaps, w_overlaps
