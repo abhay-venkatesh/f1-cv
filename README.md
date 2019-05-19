b4508b6fb2c5851d52d947cde08faf58ab3935e4
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-19 08:40:41 -0500

    working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # f1-cv
 F1 normalization with applications in computer vision.
+
+## Installation
+
+### Windows
+
+```bash
+conda env create -f environment.yml
+pip install "git+https://github.com/philferriere/cocoapi.git#egg=pycocotools&subdirectory=PythonAPI"
+```
--- lib/agents/coco_stuff_evaluator.py
+++ lib/agents/coco_stuff_evaluator.py
@@ -1,14 +1,13 @@
+# from torchvision import transforms
 from lib.agents.agent import Agent
 from lib.datasets.coco_stuff import COCOStuffEval
-from pathlib import Path
-from torch import nn
-from torch.utils.data import DataLoader
-import importlib
-import torch
 from math import floor
-from torchvision import transforms
-
-# from pycocotools.cocostuffhelper import segmentationToCocoResult
+from pathlib import Path
+from pycocotools import mask
+from torch import nn
+import importlib
+import numpy as np
+import torch
 
 
 class COCOStuffEvaluator(Agent):
@@ -52,10 +51,18 @@ class COCOStuffEvaluator(Agent):
                     _, predicted = torch.max(Y_.data, 1)
                     self._apply_w_overlaps(predicted, seg, h)
 
+                seg_array = seg.numpy()
+                anns = segmentationToCocoResult(
+                    seg_array,
+                    int(img_name.replace(".jpg", "")),
+                    stuffStartId=0)
+                print(anns)
+                """
                 seg_img = transforms.ToPILImage()(seg)
                 seg_img.save(
                     Path(self.config["outputs folder"],
                          img_name.replace(".jpg", ".png")))
+                """
 
                 raise RuntimeError
 
@@ -104,3 +111,63 @@ class COCOStuffEvaluator(Agent):
                 windows.append(img[:, h1:h2, w1:w2])
 
         return windows, h_overlaps, w_overlaps
+
+
+def segmentationToCocoResult(labelMap, imgId, stuffStartId=92):
+    '''
+    Convert a segmentation map to COCO stuff segmentation result format.
+    :param labelMap: [h x w] segmentation map that indicates the label of each
+     pixel
+    :param imgId: the id of the COCO image (last part of the file name)
+    :param stuffStartId: (optional) index where stuff classes start
+    :return: anns    - a list of dicts for each label in this image
+       .image_id     - the id of the COCO image
+       .category_id  - the id of the stuff class of this annotation
+       .segmentation - the RLE encoded segmentation of this class
+    '''
+
+    # Get stuff labels
+    shape = labelMap.shape
+    if len(shape) != 2:
+        raise Exception(
+            ('Error: Image has %d instead of 2 channels! Most likely you '
+             'provided an RGB image instead of an indexed image (with or'
+             'without color palette).') % len(shape))
+    [h, w] = shape
+    assert h > 0 and w > 0
+    labelsAll = np.unique(labelMap)
+    labelsStuff = [i for i in labelsAll if i >= stuffStartId]
+
+    # Add stuff annotations
+    anns = []
+    for labelId in labelsStuff:
+
+        # Create mask and encode it
+        Rs = segmentationToCocoMask(labelMap, labelId)
+
+        # Create annotation data and add it to the list
+        anndata = {}
+        anndata['image_id'] = int(imgId)
+        anndata['category_id'] = int(labelId)
+        anndata['segmentation'] = Rs
+        anns.append(anndata)
+    return anns
+
+
+def segmentationToCocoMask(labelMap, labelId):
+    '''
+    Encodes a segmentation mask using the Mask API.
+    :param labelMap: [h x w] segmentation map that indicates the label of each
+     pixel
+    :param labelId: the label from labelMap that will be encoded
+    :return: Rs - the encoded label mask for label 'labelId'
+    '''
+    labelMask = labelMap == labelId
+    labelMask = np.expand_dims(labelMask, axis=2)
+    labelMask = labelMask.astype('uint8')
+    labelMask = np.asfortranarray(labelMask)
+    Rs = mask.encode(labelMask)
+    assert len(Rs) == 1
+    Rs = Rs[0]
+
+    return Rs
