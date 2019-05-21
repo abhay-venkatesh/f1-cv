522ee92faff258b4a3ba35cfcf1da8737ac266be
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-21 13:04:47 -0500

    stop

--- scripts/coco_stuff_small_confidences/plot.py
+++ scripts/coco_stuff_small_confidences/plot.py
@@ -1,11 +1,27 @@
 import pandas as pd
-df = pd.read_csv('data/beta_0001_mu_003', sep=',', header=None)
-df = df.drop(0, 1)
-df.columns = ["b0001m003"]
+from pathlib import Path
+import os
 
-df_ = pd.read_csv('data/beta_0001_mu_005', sep=',', header=None)
-df_ = df_.drop(0, 1)
-df_.columns = ["b0001m005"]
-df = pd.concat([df, df_], axis=1)
 
-print(df)
+def get_data():
+    df = pd.read_csv(
+        Path("data", "f1", "beta_0001_mu_003"), sep=',', header=None)
+    df = df.drop(0, 1)
+    df.columns = ["b0001m003"]
+
+    file_names = [
+        f for f in os.listdir(Path("data", "f1"))
+        if os.path.isfile(Path("data", "f1", f))
+    ]
+    for file_name in file_names:
+        df_ = pd.read_csv(Path("data", "f1", file_name), sep=',', header=None)
+        df_ = df_.drop(0, 1)
+        df_.columns = [file_name]
+        df = pd.concat([df, df_], axis=1)
+
+    return df
+
+
+if __name__ == "__main__":
+    df = get_data()
+    print(df)
