a5e7768b448e85d2caa5ea4c451749a65f87ebda
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-21 13:32:22 -0500

    stop for now

--- scripts/coco_stuff_small_confidences/plot.py
+++ scripts/coco_stuff_small_confidences/plot.py
@@ -1,13 +1,17 @@
 import pandas as pd
 from pathlib import Path
 import os
+import seaborn as sns
+
+
+def set_styles():
+    sns.set(style="darkgrid")
 
 
 def get_data():
     df = pd.read_csv(
         Path("data", "f1", "beta_0001_mu_003"), sep=',', header=None)
-    df = df.drop(0, 1)
-    df.columns = ["b0001m003"]
+    df.columns = ["Epoch", "Mean IoU"]
 
     file_names = [
         f for f in os.listdir(Path("data", "f1"))
@@ -15,13 +19,22 @@ def get_data():
     ]
     for file_name in file_names:
         df_ = pd.read_csv(Path("data", "f1", file_name), sep=',', header=None)
-        df_ = df_.drop(0, 1)
-        df_.columns = [file_name]
-        df = pd.concat([df, df_], axis=1)
+        df_.columns = ["Epoch", "Mean IoU"]
+        df = pd.concat([df, df_], axis=0)
 
     return df
 
 
-if __name__ == "__main__":
+def plot(df):
+    plotobj = sns.lineplot(x="Epoch", y="Mean IoU", data=df)
+    plotobj.get_figure().savefig("test.png")
+
+
+def main():
     df = get_data()
-    print(df)
+    plot(df)
+
+
+if __name__ == "__main__":
+    set_styles()
+    main()
