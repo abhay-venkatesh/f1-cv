4123f65ab8de3115202e1b6920783eed7fa86a73
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-22 07:38:27 -0500

    st

--- scripts/archive/coco/coco_val/combine.py
+++ scripts/archive/coco/coco_val/combine.py
@@ -16,15 +16,15 @@ def fix_map(coco_evals):
 
 if __name__ == "__main__":
     coco_evals = []
-    for i in range(0, 10):
+    for i in range(0, 5):
         filepath = Path("coco_stuff_deep_lab_val_" + str(i), "outputs",
                         "coco_result.json")
         with open(filepath) as f:
             coco_evals.extend(json.load(f))
 
-    with open("coco_eval.json", "w+") as f:
+    with open("coco_val.json", "w+") as f:
         json.dump(coco_evals, f)
 
     coco_evals_ = fix_map(coco_evals)
-    with open("coco_eval_.json", "w+") as f:
+    with open("coco_val_.json", "w+") as f:
         json.dump(coco_evals_, f)
