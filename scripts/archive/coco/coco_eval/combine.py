c2ecef5c206d12f47c56f69020bb51114b1fe3ad
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-22 07:29:41 -0500

    stop

--- scripts/archive/coco/coco_eval/combine.py
+++ scripts/archive/coco/coco_eval/combine.py
@@ -1,6 +1,19 @@
 import simplejson as json
 from pathlib import Path
 
+
+def fix_map(coco_evals):
+    coco_evals_ = []
+    for coco_eval in coco_evals:
+        coco_eval_ = coco_eval
+        if coco_eval["category_id"] == 0:
+            coco_eval_["category_id"] = 183
+        else:
+            coco_eval_["category_id"] += 91
+        coco_evals_.append(coco_eval_)
+    return coco_evals_
+
+
 if __name__ == "__main__":
     coco_evals = []
     for i in range(0, 10):
@@ -8,5 +21,10 @@ if __name__ == "__main__":
                         "outputs", "coco_result.json")
         with open(filepath) as f:
             coco_evals.extend(json.load(f))
+
     with open("coco_eval.json", "w+") as f:
         json.dump(coco_evals, f)
+
+    coco_evals_ = fix_map(coco_evals)
+    with open("coco_eval_.json", "w+") as f:
+        json.dump(coco_evals_, f)
