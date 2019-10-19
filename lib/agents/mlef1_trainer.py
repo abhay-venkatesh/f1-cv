2029ee38a64e5b0e92e0168a976345fd1168fc2e
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-10-19 11:27:05 -0700

    full exp

--- lib/agents/mlef1_trainer.py
+++ lib/agents/mlef1_trainer.py
@@ -11,9 +11,108 @@ import torch
 import torch.nn.functional as F
 from sklearn.metrics import f1_score
 from lib.utils.functional import lagrange
+from lib.models.logistic_regression import LogisticRegression
 
 
 class MLEF1Trainer(Agent):
+    def run_baseline(self, X, Y):
+        # Use CPU for simplicity
+        self.device = torch.device("cpu")
+
+        #################
+        # Prepare dataset
+        N_CLASSES = 20
+        MEAN_N_LABELS = 14
+        N_TRAIN = 100
+        N_TEST = 100
+        X, Y = make_multilabel_classification(
+            n_samples=N_TRAIN + N_TEST, n_classes=N_CLASSES, n_labels=MEAN_N_LABELS
+        )
+        # X_train.shape = (100, 20), X_test.shape = (100, 20)
+        X_train, X_test = X[:N_TRAIN], X[N_TRAIN:]
+        # Y_train.shape = (100, 20), Y_test.shape = (100, 20)
+        Y_train, Y_test = Y[:N_TRAIN], Y[N_TRAIN:]
+
+        # We train one model per class
+        models = {}
+
+        for c in tqdm(range(N_CLASSES)):
+            self.logger = Logger(Path(self.config["stats folder"], "class_" + str(c)))
+
+            # One vs Rest Transformation
+            # (100, 20) -> (100,)
+            Y_train_c = Y_train[:, c]
+
+            # Model and optimizer
+            model = LogisticRegression().to(self.device).double()
+            optimizer = torch.optim.SGD(
+                model.parameters(), lr=self.config["learning rate"]
+            )
+
+            # Load checkpoint if exists
+            start_epochs = self._load_checkpoint(model)
+
+            # Dataset
+            for epoch in range(start_epochs, self.config["epochs"]):
+                total_loss = 0
+                model.train()
+                for X, Y in zip(X_train, Y_train_c):
+
+                    # Forward computation
+                    X, Y = torch.from_numpy(X), torch.tensor(Y).reshape((1,))
+                    # X, Y = X.to(self.device), Y.to(self.device)
+                    Y_ = model(X).reshape((1, -1))
+                    loss = F.cross_entropy(Y_, Y)
+                    total_loss += loss.item()
+
+                    # Backpropagate
+                    loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+            models[c] = model
+
+        preds = np.zeros(Y_test.shape)
+        probs = np.zeros(Y_test.shape)
+        for c in range(N_CLASSES):
+            model = models[c]
+            model.eval()
+
+            # One vs Rest Transformation
+            # (100, 20) -> (100,)
+            Y_test_c = Y_test[:, c]
+
+            with torch.no_grad():
+                pred_layer = np.zeros(Y_test_c.shape)
+                prob_layer = np.zeros(Y_test_c.shape)
+                for i, (X, Y) in enumerate(zip(X_test, Y_test_c)):
+                    X, Y = torch.from_numpy(X), torch.tensor(Y).reshape((1,))
+                    Y_ = model(X).reshape((1, -1))
+                    _, predicted = torch.max(Y_.data, 1)
+                    pred_layer[i] = predicted
+                    prob_layer[i] = torch.nn.Softmax(dim=1)(Y_)[0][1]
+
+            preds[:, c] = pred_layer
+            probs[:, c] = prob_layer
+
+        # Simple prediction
+        scores = list()
+        for x, y in zip(preds, Y_test):
+            scores.append(f1_score(x, y))
+        score_simple = np.mean(scores)
+        print("Simple score:", score_simple)
+
+        # Optimized prediction
+        scores = list()
+        for x, y in zip(probs, Y_test):
+            pred = np.zeros(N_CLASSES)
+            pred_idxs = optimal_basket(x)
+            pred[pred_idxs] = 1
+            scores.append(f1_score(pred, y))
+        score_optimized = np.mean(scores)
+        print("Optimized score:", score_optimized)
+        pass
+
     def run(self):
         #################
         # Prepare dataset
@@ -24,6 +123,13 @@ class MLEF1Trainer(Agent):
         X, Y = make_multilabel_classification(
             n_samples=N_TRAIN + N_TEST, n_classes=N_CLASSES, n_labels=MEAN_N_LABELS
         )
+        self.run_baseline(X, Y)
+        self.run_f1(X, Y)
+
+    def run_f1(self, X, Y):
+        N_CLASSES = 20
+        N_TRAIN = 100
+
         # X_train.shape = (100, 20), X_test.shape = (100, 20)
         X_train, X_test = X[:N_TRAIN], X[N_TRAIN:]
         # Y_train.shape = (100, 20), Y_test.shape = (100, 20)
@@ -176,6 +282,23 @@ class MLEF1Trainer(Agent):
                     mu.data += self.config["eta_mu"] * (mu_cache - 1)
                     lamb.data += lamb_cache.reshape((len(X_train)))
                     gamma.data += gamma_cache.reshape((len(X_train)))
+
+            # Evaluate model
+            model.eval()
+
+            # One vs Rest Transformation
+            # (100, 20) -> (100,)
+            Y_test_c = Y_test[:, c]
+
+            with torch.no_grad():
+                pred_layer = np.zeros(Y_test_c.shape)
+                for i, (X, Y) in enumerate(zip(X_test, Y_test_c)):
+                    X, Y = torch.from_numpy(X), torch.tensor(Y).reshape((1,))
+                    Y_, _ = model(X)
+                    Y_ = Y_.reshape((1, -1))
+                    _, predicted = torch.max(Y_.data, 1)
+                    pred_layer[i] = predicted
+                # print(f1_score(pred_layer, Y_test_c))
 
             models[c] = model
 
