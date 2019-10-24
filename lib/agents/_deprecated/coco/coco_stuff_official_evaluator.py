d8573da161dcda36858aac545a47acb5ce6c834a
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-10-24 10:11:32 -0700

    rerun baseline

--- lib/agents/_deprecated/coco/coco_stuff_official_evaluator.py
+++
@@ -1,7 +0,0 @@
-from lib.agents.agent import Agent
-from lib.utils.cocostuffapi.cocostuffeval import COCOStuffeval
-from pycocotools.coco import COCO
-
-
-class COCOStuffOfficialEvaluator(Agent):
-    raise NotImplementedError
